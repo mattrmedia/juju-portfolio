f8d704563d0a9d51c59f0c04b79d612dc78794ce
Matt Roe <mroedesign@gmail.com>
2022-02-27 15:25:07 -0500

    assetPrefix path to fix reload issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
-  assetPrefix: ".",
+  assetPrefix: isProd ? 'http://juliaroedesign.com/' : '',
   reactStrictMode: true,
   basePath: '',
   images: {
